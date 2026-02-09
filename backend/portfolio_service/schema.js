33b8825d65fd4a93b25f2f91bcf0c855bcbf572d
= <=>
2026-02-09 18:33:32 +0000

    portfolio schema implemented

--- backend/portfolio_service/schema.js
+++ backend/portfolio_service/schema.js
@@ -1,11 +1,29 @@
 import mongoose from 'mongoose';
 
+const StockSchema = new mongoose.Schema({
+    symbol: {
+        type: String,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    purchasePrice: {
+        type: Number,
+        required: true,
+    },
+})
+
 const potfolioSchema = new mongoose.Schema({
     userId: {
         type: String,
         required: true,
     },
-    stockSymbols: {
-        
+    stocks: {
+        type: [StockSchema],
+        required: true,
     }
 })
+
+const Portfolio = mongoose.model('Potfolio', potfolioSchema)
